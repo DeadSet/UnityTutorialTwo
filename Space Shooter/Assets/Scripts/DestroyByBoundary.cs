e77c713f7923e24433931875f94ff8f7815af26f
DeadSet <DeadSet@users.noreply.github.com>
2017-08-14 20:25:17 -0500

    Added miss penalty and hazardCount++

--- Space Shooter/Assets/Scripts/DestroyByBoundary.cs
+++ Space Shooter/Assets/Scripts/DestroyByBoundary.cs
@@ -3,9 +3,17 @@ using System.Collections;
 
 public class DestroyByBoundary : MonoBehaviour 
 {
+	public GameController gameController; //assign gamecontroller script in inspector
+	public int missPenalty; 
+
 	void OnTriggerExit(Collider other)
 	{
+		if (other.tag == "Asteroid") {
+			gameController.AddScore (missPenalty);
+		}
 		Destroy(other.gameObject);
+
+		
 	}
 
 	
--- Space Shooter/Assets/Scripts/GameController.cs
+++ Space Shooter/Assets/Scripts/GameController.cs
@@ -56,6 +56,7 @@ public class GameController : MonoBehaviour {
 				restart = true;
 				break;
 			}
+			hazardCount++;
 		}
 	}
 
